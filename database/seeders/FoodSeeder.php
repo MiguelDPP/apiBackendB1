cd74e9106fc763847e3caf2bff4c617b4a431b68
miguecode <mipor.278@gmail.com>
2023-06-07 10:10:00 -0500

    Agregamos imagenes a las comidas

--- database/seeders/FoodSeeder.php
+++ database/seeders/FoodSeeder.php
@@ -20,7 +20,8 @@ class FoodSeeder extends Seeder
             'description' => 'Sandwich de Pollo con Jamon y Queso, mas Avena de Soya',
             'count' => 20,
             'price' => '5000',
-            'type' => 'desayuno'
+            'type' => 'desayuno',
+            'featured' => 'https://www.philadelphia.com.mx/modx/assets/img/revision2016/images/recetas/recetas_2015/sandwich_de_pollo.jpg'
         ]);
 
         DB::table('food')->insert([
@@ -28,7 +29,8 @@ class FoodSeeder extends Seeder
             'description' => 'Arroz Con Pollo, Papa a la francesa y Jugo de Mora',
             'count' => 20,
             'price' => '10000',
-            'type' => 'almuerzo'
+            'type' => 'almuerzo',
+            'featured' => 'https://tofuu.getjusto.com/orioneat-prod/Cj2rBNf8YbamwPu22-Arroz%20con%20pollo.jpg'
         ]);
 
         DB::table('food')->insert([
@@ -36,7 +38,8 @@ class FoodSeeder extends Seeder
             'description' => 'Hamburguesa con Papas a la Francesa',
             'count' => 20,
             'price' => '8000',
-            'type' => 'cena'
+            'type' => 'cena',
+            'featured' => 'https://cdn2.cocinadelirante.com/sites/default/files/images/2020/08/ideas-de-hamburguesas-caseras-y-papas-fritas.jpg'
         ]);
 
         DB::table('clients')->insert([
